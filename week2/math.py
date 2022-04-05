f8c51a1d5adbbc3a740ac64960ab77dcc5a8d35b
sarayu-pr11 <55467797+sarayu-pr11@users.noreply.github.com>
2022-04-04 22:13:53 -0700

    Create math.py

---
+++ week2/math.py
@@ -0,0 +1,22 @@
+class MyMath:
+  def findFactors(self, number):
+    print("OOPFactors of a Given Number {0} are:".format(number))
+    for value in range(1, number + 1):
+      if number % value == 0:
+        print("{0}".format(value), end=" ")
+    print()
+
+def factors():
+  print("hello from factors")
+  num = int(input("Enter any Number to find its factors: "))
+  findFactors(num)
+  mat = MyMath()
+  mat.findFactors(num)
+
+
+def findFactors(number):
+  print("ImpFactors of a Given Number {0} are:".format(number))
+  for value in range(1, number + 1):
+    if number % value == 0:
+      print("{0}".format(value), end=" ")
+  print()
